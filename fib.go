0d71019325ed8e66b892bb0aceca53ffa776b620
Eric <106038230+Bettmo@users.noreply.github.com>
2023-02-03 11:21:15 +0100

    f

--- fib.go
+++ fib.go
@@ -1,5 +1,23 @@
 package fib
-
-        func Fib(n int) int {
+	/* PASS
+    func Fib(n int) int {
+      return 0
+    }
+    */
+    /* PASS
+    func Fib(n int) int {
+      if n == 0 {
           return 0
-        }
+      }
+      return 1
+    }
+	*/
+	func Fib(n int) int {
+	if n == 0 {
+		return 0
+	}
+	if (n <= 2) {
+		return 1
+	}
+	return Fib(n-1) + Fib(n-2)
+  	}
--- fib_test.go
+++ fib_test.go
@@ -4,10 +4,33 @@ package fib
 
         func TestFib(t *testing.T) {
           // Her går testen
-		  input := 0
-          got := Fib(input)
-          want := 0
-          if got != want {
-              t.Errorf("Fib(%d): want %d, got %d", input, want, got)
+		      //input := 1 // legg merke til operatør =
+          //got := Fib(input)
+          //want := 1
+          //if got != want {
+          //    t.Errorf("Fib(%d): want %d, got %d", input, want, got)
+          
+          type test struct {
+            input int
+            want int
           }
+
+          var tests = []test {
+            {0,0},
+            {1,1},
+            {2,1},
+            {3,2},
+            // her kan vi bare legge på nye test cases
+          }
+
+          // tc - test case
+          for _, tc := range tests {
+              got := Fib(tc.input)
+              if got != tc.want {
+                  t.Errorf("Fib(%d): want %d, got %d", tc.input, tc.want, got)
+              }
+          }
+        
+        
         }
+        
