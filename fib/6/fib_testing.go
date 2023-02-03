a96f9f4f7b41036d6e397b71b3d85ab54bfcb062
Eric <106038230+Bettmo@users.noreply.github.com>
2023-02-03 10:20:32 +0100

    2

--- fib/6/fib_testing.go
+++ fib/6/fib_testing.go
@@ -4,9 +4,9 @@ import "testing"
 
 func TestFib(t *testing.T) {
 	// Her går testen
-	input := 0
-	got := fib(input)
-	want := 0
+	input := 1
+	got := Fib(input)
+	want := 1
 	if got != want {
 		t.Errorf("Fib(%d) = %d; want %d", input, got, want)
 	}
